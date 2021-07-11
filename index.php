7e1d3f7b3b1db141d868066d220ebe8ff829b0f9
zahidNajmu <znajm1773@gmail.com>
2021-07-11 11:06:03 +0800

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Coba Git Remote</title>
+</head>
+<body>
+  <h1>Hello World!</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sit debitis eum alias vero ipsum hic sapiente dolor exercitationem officiis libero fuga, quaerat quisquam nemo impedit quod, sequi voluptatibus culpa!</p>
+</body>
+</html>
