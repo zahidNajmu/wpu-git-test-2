2784f9beecc540078c250703cc56a01ed5b65079
zahidNajmu <znajm1773@gmail.com>
2021-07-11 11:13:00 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sit debitis eum alias vero ipsum hic sapiente dolor exercitationem officiis libero fuga, quaerat quisquam nemo impedit quod, sequi voluptatibus culpa!</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+    <li>tiga</li>
+  </ul>
 </body>
 </html>
